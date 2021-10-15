eab8c7e463dd8b7bf6aa1f620d9c9a6b4f9d6ff6
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2021-10-15 09:45:40 -0500

    Clase Main con prubas sencillas de los métodos del árbol binario.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -16,18 +16,28 @@ public class Main {
 		tree.createNode(80);
 		tree.createNode(96);
 				
+		System.out.println("Recorrer en pre-orden: Estado inicial");
 		tree.recorrerPreorden(tree.getRoot());
 		
 		System.out.println("__________________");
+		System.out.println("Eliminar nodo con valor 68");
 		tree.deleteNode(tree.getRoot(),68);
 		tree.recorrerPreorden(tree.getRoot());
 		
 		System.out.println("__________________");
+		System.out.println("Eliminar nodo con valor 66");
 		tree.deleteNode(tree.getRoot(),66);
 		tree.recorrerPreorden(tree.getRoot());
 		
 		System.out.println("__________________");
-		System.out.println("Get value of a node: "+tree.getTreeNode(tree.getRoot(), 80).getValue());
+		System.out.println("Obtener valor de un nodo: "+tree.getTreeNode(tree.getRoot(), 80).getValue());
+		
+		System.out.println("__________________");		
+		System.out.println("¿Está vacío el árbol?: "+tree.isEmpty());
+		
+		System.out.println("__________________");		
+		System.out.println("¿El nodo con valor 80 es una hoja?: "+tree.isLeaf(tree.getTreeNode(tree.getRoot(), 80)));
+		
 
 
 	}
